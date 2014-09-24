0262a9a4ff542270fcd82779e12d1b5a9f89c8d3
Matthew Brett <matthew.brett@gmail.com>
2014-09-24 18:55:00 -0400

    NF: add working-on-laptop directives.

--- sphinxext/prizefiles.py
+++ sphinxext/prizefiles.py
@@ -29,11 +29,30 @@ class PrizeVars(AddVars):
     default_cwd = 'nobel_prize'
 
 
+class LaptopRun(RunBlock):
+    prompt_prefix = '[laptop]$ '
+    default_cwd = 'my_repos'
+    default_home = '/fake_home'
+
+
+class PrizeLapRun(LaptopRun):
+    default_cwd = 'my_repos/nobel_prize'
+
+
+class PrizeLapCommit(RunCommit):
+    prompt_prefix = '[laptop]$ '
+    default_home = '/fake_home'
+    default_cwd = 'my_repos/nobel_prize'
+
+
 def setup(app):
     app.add_directive('desktoprun', DesktopRun)
     app.add_directive('prizerun', PrizeRun)
     app.add_directive('prizecommit', PrizeCommit)
     app.add_directive('prizevars', PrizeVars)
     app.add_directive('prizewrite', PrizeWrite)
+    app.add_directive('laptoprun', LaptopRun)
+    app.add_directive('prizelaprun', PrizeLapRun)
+    app.add_directive('prizelapcommit', PrizeLapCommit)
 
 # vim: set expandtab shiftwidth=4 softtabstop=4 :
